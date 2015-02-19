8c1cdb1b616631f4511b2c264789ccf65eb9acf1
jpdef <jacobdefilippis@gmail.com>
2015-02-18 16:07:32 -0800

    arduino makefile fixes

--- Arduino_Files/read_write/build-cli/depends.mk
+++ Arduino_Files/read_write/build-cli/depends.mk
@@ -1,8 +1,4 @@
-<<<<<<< .merge_file_OVoiBs
-build-cli/read_write.o: build-cli/read_write.cpp \
-=======
 build-cli/ultrasonic_example.o: build-cli/ultrasonic_example.cpp \
->>>>>>> .merge_file_l1fPxo
  /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
  /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
  /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
@@ -12,9 +8,5 @@ build-cli/ultrasonic_example.o: build-cli/ultrasonic_example.cpp \
  /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
  /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
  /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
-<<<<<<< .merge_file_OVoiBs
- /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h
-=======
  /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
  /usr/share/arduino/libraries/QueueList/QueueList.h
->>>>>>> .merge_file_l1fPxo
